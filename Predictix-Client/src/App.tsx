60f41f950d1602d25bdf1342f29c2e59cc85d2ff
ShirelO98 <shirelo983@gmail.com>
2024-12-23 19:17:00 +0200

    Init Project

---
+++ Predictix-Client/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import { DashboardLayoutSidebarCollapsed as Dashboard } from "./components/Dashboard";
+
+function App() {
+  return (
+    <>
+      <Dashboard />
+    </>
+  );
+}
+
+export default App;
