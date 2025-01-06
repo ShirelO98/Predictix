9724a56e3e168cbff74b8191f21d4af5ef2d359b
AdarAfek <adar257@gmail.com>
2025-01-06 20:15:46 +0200

    Update App.tsx

--- Predictix-Client/src/App.tsx
+++ Predictix-Client/src/App.tsx
@@ -1,29 +1,33 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend"; // For browser-based drag-and-drop
 import LoginPage from "./components/Login/LoginPage";
 import Dashboard from "./components/Dashboard/Dashboard";
 import ProtectedRoute from "./components/Dashboard/ProtectedRoute";
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        {/* Login */}
-        <Route path="/login" element={<LoginPage />} />
+    <DndProvider backend={HTML5Backend}>
+      <Router>
+        <Routes>
+          {/* Login */}
+          <Route path="/login" element={<LoginPage />} />
 
-        {/* Protected Dashbord */}
-        <Route
-          path="/dashboard/*"
-          element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
-          }
-        />
+          {/* Protected Dashboard */}
+          <Route
+            path="/dashboard/*"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
 
-        {/* Default path*/}
-        <Route path="*" element={<LoginPage />} />
-      </Routes>
-    </Router>
+          {/* Default Path */}
+          <Route path="*" element={<LoginPage />} />
+        </Routes>
+      </Router>
+    </DndProvider>
   );
 }
